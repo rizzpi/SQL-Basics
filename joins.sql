0716a31cecb640b8566377cb2f9a78a463d02f8b
Rshijuta Pokharel <71976474+rizzpi@users.noreply.github.com>
2022-06-09 16:40:48 +0545

    Add files via upload

---
+++ joins.sql
@@ -0,0 +1,419 @@
+/*intro to joins*/
+
+ALTER TABLE dept_dup
+
+DROP COLUMN dept_manager;
+
+ALTER TABLE dept_dup
+
+CHANGE COLUMN dept_no dept_no CHAR(4) NULL;
+
+ALTER TABLE dept_dup
+CHANGE COLUMN dept_name dept_name VARCHAR(40) NULL;
+
+select * from dept_dup order by dept_no asc;
+
+DROP TABLE IF EXISTS dept_manager_dup;
+rollback;
+
+CREATE TABLE dept_manager_dup (
+
+  emp_no int(11) NOT NULL,
+
+  dept_no char(4) NULL,
+
+  from_date date NOT NULL,
+
+  to_date date NULL
+
+  );
+INSERT INTO dept_manager_dup
+
+select * from dept_manager_dup;
+
+select * from dept_manager_dup;
+
+INSERT INTO dept_manager_dup (emp_no, from_date)
+
+VALUES                (999904, '2017-01-01'),(999905, '2017-01-01'),(999906, '2017-01-01'),
+
+                               (999907, '2017-01-01');
+
+DELETE FROM dept_manager_dup 
+WHERE
+    dept_no = 'd001';
+    
+INSERT INTO dept_dup (dept_name)
+
+VALUES                ('Public Relations');
+
+DELETE FROM dept_dup 
+WHERE
+    dept_no = 'd002'; 
+
+ALTER TABLE dept_dup RENAME TO departments_dup; 
+
+
+/* INNER JOINS */
+
+SELECT 
+    *
+FROM
+    dept_manager_dup
+ORDER BY dept_no;
+
+-- departments_dup
+SELECT 
+    *
+FROM
+    departments_dup
+ORDER BY dept_no;
+
+SELECT 
+    departments_dup.dept_no, dept_manager_dup.dept_no
+FROM
+    departments_dup
+        JOIN
+    dept_manager_dup ON departments_dup.dept_no = dept_manager_dup.dept_no;
+    
+SELECT 
+    a.dept_no, b.dept_no
+FROM
+    departments_dup a
+        JOIN
+    dept_manager_dup b ON a.dept_no = b.dept_no;
+
+-- LECTURE: INNER JOIN - Part II
+
+SELECT 
+    m.dept_no, m.emp_no, d.dept_name
+FROM
+    dept_manager_dup m
+        INNER JOIN
+    departments_dup d ON m.dept_no = d.dept_no
+ORDER BY m.dept_no;
+
+SELECT 
+    m.emp_no, e.first_name, e.last_name, m.dept_no, e.hire_date
+FROM
+    dept_manager m
+        INNER JOIN
+    employees e ON m.emp_no = e.emp_no
+ORDER BY m.emp_no ASC;
+
+SELECT 
+    e.emp_no, e.first_name, e.last_name, dm.dept_no, e.hire_date
+FROM
+    employees e
+        JOIN
+    dept_manager dm ON e.emp_no = dm.emp_no
+ORDER BY dm.emp_no ASC;
+
+-- LECTURE: A Note on Using Joins
+-- add m.to_date and d.dept_name
+SELECT 
+    m.dept_no, m.emp_no, m.from_date, m.to_date, d.dept_name
+FROM
+    dept_manager_dup m
+		INNER JOIN
+    departments_dup d ON m.dept_no = d.dept_no
+ORDER BY m.dept_no;
+
+-- LECTURE: Duplicate Records
+
+-- duplicate records
+INSERT INTO dept_manager_dup 
+VALUES 	('110228', 'd003', '1992-03-21', '9999-01-01');
+        
+INSERT INTO departments_dup 
+VALUES	('d009', 'Customer Service');
+
+-- dept_manager_dup
+SELECT 
+    *
+FROM
+    dept_manager_dup
+ORDER BY dept_no ASC;
+
+-- departments_dup
+SELECT 
+    *
+FROM
+    departments_dup
+ORDER BY dept_no ASC;
+
+-- inner join
+SELECT 
+    m.dept_no, m.emp_no, d.dept_name
+FROM
+    dept_manager_dup m
+        INNER JOIN
+    departments_dup d ON m.dept_no = d.dept_no
+ORDER BY dept_no;
+
+-- add GROUP BY m.emp_no
+SELECT 
+    m.dept_no, m.emp_no, d.dept_name
+FROM
+    dept_manager_dup m
+        JOIN
+    departments_dup d ON m.dept_no = d.dept_no
+GROUP BY m.emp_no
+ORDER BY dept_no;
+
+-- remove the duplicates from the two tables
+DELETE FROM dept_manager_dup 
+WHERE emp_no = '110228';
+DELETE FROM departments_dup 
+WHERE dept_no = 'd009';
+
+-- add back the initial records
+INSERT INTO dept_manager_dup 
+VALUES 	('110228', 'd003', '1992-03-21', '9999-01-01');
+        
+INSERT INTO departments_dup 
+VALUES	('d009', 'Customer Service');
+
+-- left join
+SELECT 
+    m.dept_no, m.emp_no, d.dept_name
+FROM
+    dept_manager_dup m
+        LEFT JOIN
+    departments_dup d ON m.dept_no = d.dept_no
+GROUP BY m.emp_no
+ORDER BY m.dept_no;
+
+
+-- d LEFT JOIN m
+SELECT 
+    m.dept_no, m.emp_no, d.dept_name
+FROM
+    departments_dup d
+		LEFT JOIN
+    dept_manager_dup m ON m.dept_no = d.dept_no
+ORDER BY m.dept_no;
+
+-- SELECT d.dept_no
+SELECT 
+    d.dept_no, m.emp_no, d.dept_name
+FROM
+    departments_dup d
+		LEFT JOIN
+    dept_manager_dup m ON m.dept_no = d.dept_no
+ORDER BY d.dept_no;
+
+-- LEFT OUTER JOIN
+SELECT 
+    d.dept_no, m.emp_no, d.dept_name
+FROM
+    departments_dup d
+        LEFT OUTER JOIN
+    dept_manager_dup m ON m.dept_no = d.dept_no
+ORDER BY d.dept_no;
+
+SELECT 
+    e.emp_no, e.first_name, e.last_name, m.dept_no, m.from_date
+FROM
+    employees e
+        LEFT JOIN
+    dept_manager m ON e.emp_no = m.emp_no
+WHERE
+    e.last_name = 'Markovitch'
+ORDER BY m.dept_no DESC , e.emp_no;
+
+SELECT 
+    e.emp_no, e.first_name, e.last_name, s.salary
+FROM
+    employees e
+        JOIN
+    salaries s ON e.emp_no = s.emp_no
+WHERE
+    s.salary > 145000;
+
+-- Old Join Syntax
+SELECT 
+    e.emp_no,
+    e.first_name,
+    e.last_name,
+    dm.dept_no,
+    e.hire_date
+FROM
+    employees e,
+    dept_manager dm
+WHERE
+    e.emp_no = dm.emp_no;
+
+-- New Join Syntax:
+SELECT 
+    e.emp_no,
+    e.first_name,
+    e.last_name,
+    dm.dept_no,
+    e.hire_date
+FROM
+    employees e
+        JOIN
+    dept_manager dm ON e.emp_no = dm.emp_no; 
+
+set @@global.sql_mode := replace(@@global.sql_mode, 'ONLY_FULL_GROUP_BY', '');
+
+-- EXERCISE 1: JOIN and WHERE Used Together
+
+SELECT 
+    e.first_name, e.last_name, e.hire_date, t.title
+FROM
+    employees e
+        JOIN
+    titles t ON e.emp_no = t.emp_no
+WHERE
+    e.first_name = 'Margareta'
+        AND e.last_name = 'Markovitch'
+ORDER BY e.emp_no;
+
+-- LECTURE: CROSS JOIN
+
+SELECT 
+    dm.*, d.*
+FROM
+    dept_manager dm
+        CROSS JOIN
+    departments d
+ORDER BY dm.emp_no , d.dept_no;
+
+SELECT 
+    dm.*, d.*
+FROM
+    dept_manager dm,
+    departments d
+ORDER BY dm.emp_no , d.dept_no;
+
+SELECT 
+    e.*, d.*
+FROM
+    departments d
+        CROSS JOIN
+    dept_manager dm
+        JOIN
+    employees e ON dm.emp_no = e.emp_no
+WHERE
+    d.dept_no <> dm.dept_no
+ORDER BY dm.emp_no , d.dept_no
+;
+
+SELECT 
+    dm.*, d.*
+FROM
+    dept_manager dm
+        CROSS JOIN
+    departments d
+WHERE
+    d.dept_no = 'd009'
+ORDER BY d.dept_no;
+
+SELECT DISTINCT
+    e.*, d.*
+FROM
+    employees e
+        CROSS JOIN
+    departments d
+WHERE
+    e.emp_no < 10011
+ORDER BY e.emp_no, d.dept_no;
+
+-- LECTURE: Join more than Two Tables in SQL
+
+SELECT 
+    e.last_name,
+    e.first_name,
+    e.hire_date,
+    m.from_date,
+    d.dept_name
+FROM
+    employees e
+        JOIN
+    dept_manager m ON e.emp_no = m.emp_no
+        JOIN
+    departments d ON m.dept_no = d.dept_no
+;
+
+SELECT 
+    e.first_name,
+    e.last_name,
+    e.hire_date,
+    t.title,
+    dm.from_date,
+    d.dept_name
+FROM
+    employees e
+        JOIN
+    dept_manager dm ON e.emp_no = dm.emp_no
+        JOIN
+    departments d ON dm.dept_no = d.dept_no
+        JOIN
+    titles t ON t.emp_no = e.emp_no
+WHERE
+    t.title = 'Manager'
+ORDER BY e.emp_no;
+
+-- add GROUP BY d.dept_name
+SELECT 
+    d.dept_name, AVG(salary)
+FROM
+    departments d
+        JOIN
+    dept_manager m ON d.dept_no = m.dept_no
+        JOIN
+    salaries s ON m.emp_no = s.emp_no
+GROUP BY d.dept_name
+;
+
+
+-- add HAVING average_salary > 60000
+SELECT 
+    d.dept_name, AVG(salary) AS average_salary
+FROM
+    departments d
+        JOIN
+    dept_manager m ON d.dept_no = m.dept_no
+        JOIN
+    salaries s ON m.emp_no = s.emp_no
+GROUP BY dept_name
+HAVING average_salary > 60000
+ORDER BY average_salary DESC
+;
+ 
+SELECT 
+    e.gender, COUNT(e.emp_no) AS gender
+FROM
+    employees e
+        JOIN
+    titles t ON e.emp_no = t.emp_no
+WHERE
+    title = 'Manager'
+GROUP BY gender;
+
+-- alternate soln
+
+SELECT 
+    e.gender, COUNT(dm.emp_no)
+FROM
+    employees e
+        JOIN
+    dept_manager dm ON e.emp_no = dm.emp_no
+GROUP BY gender;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
