48e7f6711137957ac33fd413f58d33b65e6bdce9
Rshijuta Pokharel <71976474+rizzpi@users.noreply.github.com>
2022-06-09 16:58:43 +0545

    Add files via upload

---
+++ union vs. union all.sql
@@ -0,0 +1,93 @@
+/* Create a employees duplicate table for UNION vs UNION ALL
+DROP TABLE IF EXISTS employees_dup;
+CREATE TABLE employees_dup (
+   emp_no int(11),
+   birth_date date,
+   first_name varchar(14),
+   last_name varchar(16),
+   gender enum('M','F'),
+   hire_date date
+  );
+  
+INSERT INTO employees_dup 
+SELECT 
+    e.*
+FROM
+    employees e
+LIMIT 20;
+
+-- Check
+SELECT 
+    *
+FROM
+    employees_dup
+;
+
+INSERT INTO employees_dup VALUES
+('10001', '1953-09-02', 'Georgi', 'Facello', 'M', '1986-06-26');
+
+*/
+
+-- UNION ALL
+SELECT 
+    e.emp_no,
+    e.first_name,
+    e.last_name,
+    NULL AS dept_no,
+    NULL AS from_date
+FROM
+    employees_dup e
+WHERE
+    e.emp_no = 10001 
+UNION ALL SELECT 
+    NULL AS emp_no,
+    NULL AS first_name,
+    NULL AS last_name,
+    m.dept_no,
+    m.from_date
+FROM
+    dept_manager m;
+
+-- UNION
+SELECT 
+    e.emp_no,
+    e.first_name,
+    e.last_name,
+    NULL AS dept_no,
+    NULL AS from_date
+FROM
+    employees_dup e
+WHERE
+    e.emp_no = 10001 
+UNION SELECT 
+    NULL AS emp_no,
+    NULL AS first_name,
+    NULL AS last_name,
+    m.dept_no,
+    m.from_date
+FROM
+    dept_manager m;
+
+-- EXERCISE 1: UNION vs UNION ALL
+SELECT 
+    *
+FROM
+    (SELECT 
+        e.emp_no,
+            e.first_name,
+            e.last_name,
+            NULL AS dept_no,
+            NULL AS from_date
+    FROM
+        employees e
+    WHERE
+        last_name = 'Denis' UNION SELECT 
+        NULL AS emp_no,
+            NULL AS first_name,
+            NULL AS last_name,
+            dm.dept_no,
+            dm.from_date
+    FROM
+        dept_manager dm) a
+ORDER BY -a.emp_no DESC;
+
