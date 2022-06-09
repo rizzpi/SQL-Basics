b2a59e94fc719b7206b90c87de7d4363551f38c6
Rshijuta Pokharel <71976474+rizzpi@users.noreply.github.com>
2022-06-09 14:47:24 +0545

    Add files via upload

--- joins.sql
+++ joins.sql
@@ -174,6 +174,176 @@ VALUES 	('110228', 'd003', '1992-03-21', '9999-01-01');
 INSERT INTO departments_dup 
 VALUES	('d009', 'Customer Service');
 
+-- left join
+SELECT 
+    m.dept_no, m.emp_no, d.dept_name
+FROM
+    dept_manager_dup m
+        LEFT JOIN
+    departments_dup d ON m.dept_no = d.dept_no
+GROUP BY m.emp_no
+ORDER BY m.dept_no;
+
+
+-- d LEFT JOIN m
+SELECT 
+    m.dept_no, m.emp_no, d.dept_name
+FROM
+    departments_dup d
+		LEFT JOIN
+    dept_manager_dup m ON m.dept_no = d.dept_no
+ORDER BY m.dept_no;
+
+-- SELECT d.dept_no
+SELECT 
+    d.dept_no, m.emp_no, d.dept_name
+FROM
+    departments_dup d
+		LEFT JOIN
+    dept_manager_dup m ON m.dept_no = d.dept_no
+ORDER BY d.dept_no;
+
+-- LEFT OUTER JOIN
+SELECT 
+    d.dept_no, m.emp_no, d.dept_name
+FROM
+    departments_dup d
+        LEFT OUTER JOIN
+    dept_manager_dup m ON m.dept_no = d.dept_no
+ORDER BY d.dept_no;
+
+SELECT 
+    e.emp_no, e.first_name, e.last_name, m.dept_no, m.from_date
+FROM
+    employees e
+        LEFT JOIN
+    dept_manager m ON e.emp_no = m.emp_no
+WHERE
+    e.last_name = 'Markovitch'
+ORDER BY m.dept_no DESC , e.emp_no;
+
+SELECT 
+    e.emp_no, e.first_name, e.last_name, s.salary
+FROM
+    employees e
+        JOIN
+    salaries s ON e.emp_no = s.emp_no
+WHERE
+    s.salary > 145000;
+
+-- Old Join Syntax
+SELECT 
+    e.emp_no,
+    e.first_name,
+    e.last_name,
+    dm.dept_no,
+    e.hire_date
+FROM
+    employees e,
+    dept_manager dm
+WHERE
+    e.emp_no = dm.emp_no;
+
+-- New Join Syntax:
+SELECT 
+    e.emp_no,
+    e.first_name,
+    e.last_name,
+    dm.dept_no,
+    e.hire_date
+FROM
+    employees e
+        JOIN
+    dept_manager dm ON e.emp_no = dm.emp_no; 
+
+set @@global.sql_mode := replace(@@global.sql_mode, 'ONLY_FULL_GROUP_BY', '');
+
+-- EXERCISE 1: JOIN and WHERE Used Together
+
+SELECT 
+    e.first_name, e.last_name, e.hire_date, t.title
+FROM
+    employees e
+        JOIN
+    titles t ON e.emp_no = t.emp_no
+WHERE
+    e.first_name = 'Margareta'
+        AND e.last_name = 'Markovitch'
+ORDER BY e.emp_no;
+
+-- LECTURE: CROSS JOIN
+
+SELECT 
+    dm.*, d.*
+FROM
+    dept_manager dm
+        CROSS JOIN
+    departments d
+ORDER BY dm.emp_no , d.dept_no;
+
+SELECT 
+    dm.*, d.*
+FROM
+    dept_manager dm,
+    departments d
+ORDER BY dm.emp_no , d.dept_no;
+
+SELECT 
+    e.*, d.*
+FROM
+    departments d
+        CROSS JOIN
+    dept_manager dm
+        JOIN
+    employees e ON dm.emp_no = e.emp_no
+WHERE
+    d.dept_no <> dm.dept_no
+ORDER BY dm.emp_no , d.dept_no
+;
+
+SELECT 
+    dm.*, d.*
+FROM
+    dept_manager dm
+        CROSS JOIN
+    departments d
+WHERE
+    d.dept_no = 'd009'
+ORDER BY d.dept_no;
+
+SELECT DISTINCT
+    e.*, d.*
+FROM
+    employees e
+        CROSS JOIN
+    departments d
+WHERE
+    e.emp_no < 10011
+ORDER BY e.emp_no, d.dept_no;
+
+-- LECTURE: Join more than Two Tables in SQL
+
+SELECT 
+    e.first_name,
+    e.last_name,
+    e.hire_date,
+    m.from_date,
+    d.dept_name
+FROM
+    employees e
+        JOIN
+    dept_manager m ON e.emp_no = m.emp_no
+        JOIN
+    departments d ON m.dept_no = d.dept_no
+;
+
+
+
+
+
+
+
+
 
 
 
